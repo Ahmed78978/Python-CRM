d6c9c89005132df7f2057f4327f4d636a556cb20
Ahmed78978 <156456149+dropella-dev@users.noreply.github.com>
2024-04-02 19:12:00 +0500

    Update app.py

--- app.py
+++ app.py
@@ -212,6 +212,7 @@ def fetch_new_emails():
     emails=[]
     for email in new_emails:
             print(email.plain, flush=True)
+            print('printing mails', flush=True)
             if email.id not in previous_email_ids:
                 previous_email_ids.add(email.id)
                 print(email.plain, flush=True)
